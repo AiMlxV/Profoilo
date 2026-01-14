bbcd7515d24d8dbe88d4aaeded23626c7916c5af
AiMlxV <41025@chs.ac.th>
2026-01-14 22:35:24 +0700

    fix: change how images stored to make downloading faster

--- data/certificates.js
+++ data/certificates.js
@@ -3,145 +3,145 @@ export const certificates = [
     {
         title: "Talent RAC",
         date: "2023",
-        image: "public/img/Cer/fibo.jpg",
+        image: "public/img/Cer/fibo.webp",
         tags: ["robotics", "camp"]
     },
     {
         title: "NextGen AI Camp",
         date: "2024",
-        image: "public/img/Cer/nextgen.jpg",
+        image: "public/img/Cer/nextgen.webp",
         tags: ["ai", "camp", "coding"]
     },
     {
         title: "Thailand Cyber Top Talent 2025",
         date: "2025",
-        image: "public/img/Cer/ctf2025.jpg",
+        image: "public/img/Cer/ctf2025.webp",
         tags: ["coding", "contest"]
     },
     {
         title: "CP LINK 2025 - Staff",
         date: "2025",
-        image: "public/img/Cer/cplink.jpg",
+        image: "public/img/Cer/cplink.webp",
         tags: ["camp"]
     },
     {
         title: "DEPA Coding Bootcamp",
         date: "2024",
-        image: "public/img/Cer/depa.jpg",
+        image: "public/img/Cer/depa.webp",
         tags: ["coding", "camp"]
     },
     {
         title: "ผ่านการอบรม สอวน. ค่าย 2",
         date: "2023",
-        image: "public/img/Cer/posn2.jpg",
+        image: "public/img/Cer/posn2.webp",
         tags: ["coding", "contest", "camp"]
     },
     {
         title: "ผ่านการอบรม สอวน. ค่าย 1",
         date: "2022",
-        image: "public/img/Cer/posn1.jpg",
+        image: "public/img/Cer/posn1.webp",
         tags: ["coding", "contest", "camp"]
     },
     {
         title: "คณะกรรมการดำเนินงาน ค่ายสอวน.",
         date: "2024",
-        image: "public/img/Cer/posn-staff.jpg",
+        image: "public/img/Cer/posn-staff.webp",
         tags: ["coding", "camp"]
     },
     {
         title: "ผู้ช่วยวิทยากรอบรม กิจกรรมพัฒนาผู้เรียน",
         date: "2024",
-        image: "public/img/Cer/saint-staff.jpg",
+        image: "public/img/Cer/saint-staff.webp",
         tags: ["stem", "camp"]
     },
     {
         title: "Bangmod Hackathon",
         date: "2023",
-        image: "public/img/Cer/bangmod.jpg",
+        image: "public/img/Cer/bangmod.webp",
         tags: ["coding", "contest"]
     },
     {
         title: "ศิลปหัตถกรรม แข่งเขียนโปรแกรม",
         date: "2022",
-        image: "public/img/Cer/com-art.jpg",
+        image: "public/img/Cer/com-art.webp",
         tags: ["coding", "contest"]
     },
     {
         title: "ศิลปหัตถกรรม หุ่นยนต์ระดับสูง",
         date: "2022",
-        image: "public/img/Cer/robo-art.png",
+        image: "public/img/Cer/robo-art.webp",
         tags: ["robotics", "contest"]
     },
     {
         title: "แข่งเขียนโปรแกรม สัปดาห์วิทยาศาสตร์มหาวิทยาลัยนเรศวร",
         date: "2024",
-        image: "public/img/Cer/nusci67.jpg",
+        image: "public/img/Cer/nusci67.webp",
         tags: ["coding", "contest"]
     },
     {
         title: "ผ่านการอบรม Dashboards Design & Data Analysis",
         date: "2023",
-        image: "public/img/Cer/digi.jpg",
+        image: "public/img/Cer/digi.webp",
         tags: ["contest", "camp"]
     },
     {
         title: "ผ่านการอบรม Up/ReSkill AIoT",
         date: "2023",
-        image: "public/img/Cer/upskillre.jpg",
+        image: "public/img/Cer/upskillre.webp",
         tags: ["ai", "robotics"]
     },
     {
         title: "Introduction to AR",
         date: "2023",
-        image: "public/img/Cer/fibo-ar.png",
+        image: "public/img/Cer/fibo-ar.webp",
         tags: ["robotics"]
     },
     {
         title: "Engineering Circuit Analysis",
         date: "2023",
-        image: "public/img/Cer/fibo-ec.png",
+        image: "public/img/Cer/fibo-ec.webp",
         tags: ["robotics"]
     },
     {
         title: "Introduction to Robotics",
         date: "2023",
-        image: "public/img/Cer/fibo-intro.png",
+        image: "public/img/Cer/fibo-intro.webp",
         tags: ["robotics"]
     },
     {
         title: "Internet of Things Appications",
         date: "2023",
-        image: "public/img/Cer/fibo-iot.png",
+        image: "public/img/Cer/fibo-iot.webp",
         tags: ["robotics"]
     },
     {
         title: "Introduction to MQTT",
         date: "2023",
-        image: "public/img/Cer/fibo-mqtt.png",
+        image: "public/img/Cer/fibo-mqtt.webp",
         tags: ["robotics"]
     },
     {
         title: "Introduction to Programming with Python",
         date: "2024",
-        image: "public/img/Cer/samsung.jpg",
+        image: "public/img/Cer/samsung.webp",
         tags: ["coding"]
     },
     {
         title: "เข้าร่วมค่าย RAI Ventures รอบ Onsite",
         date: "2023",
-        image: "public/img/Cer/RAIven.jpg",
+        image: "public/img/Cer/RAIven.webp",
         tags: ["ai", "camp", "coding"]
     },
     {
         title: "เข้าร่วมแข่งขัน TNI it Camp 2023",
         date: "2022",
-        image: "public/img/Cer/TNI.jpg",
+        image: "public/img/Cer/TNI.webp",
         tags: ["camp", "contest"]
     },
     {
         title: "เข้าร่วมค่าย ห้องเรียนพิเศษ SMTE",
         date: "2022",
-        image: "public/img/Cer/smte-camp.jpg",
+        image: "public/img/Cer/smte-camp.webp",
         tags: ["camp", "stem"]
     },
 
